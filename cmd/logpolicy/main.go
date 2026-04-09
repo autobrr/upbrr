c71b61a666daa9e9e6e8114da7cdbe67b365ac60
Audionut <audionut@riseup.net>
2026-04-09 20:25:26 +1000

    tasks

--- cmd/logpolicy/main.go
+++ cmd/logpolicy/main.go
@@ -2,29 +2,35 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 
 	"github.com/autobrr/upbrr/internal/logpolicy"
 )
 
 func main() {
-	root, err := os.Getwd()
+	os.Exit(run(os.Stdout, os.Stderr, os.Getwd, logpolicy.CheckRepository))
+}
+
+func run(stdout io.Writer, stderr io.Writer, getwd func() (string, error), check func(string) ([]logpolicy.Violation, error)) int {
+	root, err := getwd()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "logpolicy: determine working directory: %v\n", err)
-		os.Exit(1)
+		fmt.Fprintf(stderr, "logpolicy: determine working directory: %v\n", err)
+		return 1
 	}
 
-	violations, err := logpolicy.CheckRepository(root)
+	violations, err := check(root)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "logpolicy: %v\n", err)
-		os.Exit(1)
+		fmt.Fprintf(stderr, "logpolicy: %v\n", err)
+		return 1
 	}
 	if len(violations) == 0 {
-		return
+		fmt.Fprintln(stdout, "logpolicy: no issues found")
+		return 0
 	}
 
 	for _, violation := range violations {
-		fmt.Fprintf(os.Stderr, "%s:%d:%d: %s\n", violation.File, violation.Line, violation.Column, violation.Message)
+		fmt.Fprintf(stderr, "%s:%d:%d: %s\n", violation.File, violation.Line, violation.Column, violation.Message)
 	}
-	os.Exit(1)
+	return 1
 }
